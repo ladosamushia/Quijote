fb0b4d8767dc54418e878de4669249096edee617
ladosamushia <colkhis@gmail.com>
2019-10-29 20:33:33 -0500

    change trunc -> floor, fix sign

--- VinZ.jl
+++ VinZ.jl
@@ -1,10 +1,12 @@
+using DelimitedFiles
+using Base.Threads
+
 function read_binary_array(N, file)
-    X = Vector{UInt8}(undef, 496)
+    X = Vector{UInt8}(undef, N*4)
     readbytes!(file, X, N*4)
-    X = reinterpret(Float32, N*4)
+    X = reinterpret(Float32, X)
 end
-# snapfile = "C:/Users/Lado/Documents/Globus/snap_002.0"
-# Ngal = 16596561
+
 function load_snap(snapfile, Ngal)
     snapfile = open(snapfile, "r")
     # Read header
@@ -35,14 +37,14 @@ end
 #Vin_bin = zeros(Int32, 100, 100)
 
 function get_vinz(Ngal, X, Y, Z, VZ, distmax, VinZmin, VinZmax)
-    Vin_bin = zeros(Int32, nthreads(), 100, 100)
+    Vin_bin = zeros(Int32, nthreads(), 100, 200)
     @threads for i in 1:Ngal
         for j in i:Ngal
-            dist = trunc(Int32, sqrt((X[i] - X[j])^2 + (Y[i] - Y[j])^2 + (Z[i] - Z[j])^2)) + 1
+            dist = floor(Int32, sqrt((X[i] - X[j])^2 + (Y[i] - Y[j])^2 + (Z[i] - Z[j])^2)) + 1
             if dist > distmax
                 continue
             end
-            VinZ = trunc(Int32, (VZ[i] - VZ[j])*sign(Z[i] - Z[j])) + 1
+            VinZ = floor(Int32, (VZ[i] - VZ[j])*sign(Z[j] - Z[i])) + 1
             if VinZ > VinZmax || VinZ < VinZmin
                 continue
             end
@@ -51,3 +53,19 @@ function get_vinz(Ngal, X, Y, Z, VZ, distmax, VinZmin, VinZmax)
     end
     return Vin_bin
 end
+
+function main()
+    snapfile = "/home/lado/snap_002.0"
+    Ngal = 16596561
+    X, Y, Z, VX, VY, VZ = load_snap(snapfile, Ngal)
+    X = X[1:100:end]
+    Y = Y[1:100:end]
+    Z = Z[1:100:end]
+    VX = VX[1:100:end]
+    Vin_bin = get_vinz(size(X)[1], Z, Y, X, VX, 100, 1, 200)
+#    Vin_bin = sum(Vin_bin, dims=1)
+#    Vin_bin = reshape(Vin_bin, (100, 200))
+    writedlm("VinZ.csv", Vin_bin)
+end
+
+main()
